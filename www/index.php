e532e624691b9e3178bbbb4aadea5cc9c77f5ca6
Domamzn <152518279+Domamzn@users.noreply.github.com>
2024-06-17 16:02:53 +0200

    Add files via upload

---
+++ www/index.php
@@ -0,0 +1,28 @@
+<?php
+
+    $username = "billal";
+    $password = "123456";
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+		<h2>Login</h2>
+		<form>
+			<label for="username">Username:</label>
+			<input type="text" id="username" class="input-field" required><br><br>
+			<label for="password">Password:</label>
+			<input type="password" id="password" class="input-field" required><br><br>
+			<input type="submit" value="Login" class="button">
+		</form>
+	</div>
+    
+</body>
+</html>
